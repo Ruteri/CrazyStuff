fa43b356a097d37331726639330b928e1a030de7
Mateusz Morusiewicz <mmorusiewicz@newterm.pl>
2017-05-13 23:52:07 +0200

    Added generic lambda templated visitor

--- polymorphic_vector.cpp
+++ polymorphic_vector.cpp
@@ -4,48 +4,79 @@
 #include <iostream>
 
 struct Base {
-	virtual void print() = 0;
 };
 
 struct A: public Base {
-	void print() { std::cout << "struct A" << std::endl; }
+	A(int a): name("struct A"), a(a) {}
+
+	std::string name;
+	int a;
+
+	void print() const { std::cout << this << name << a << std::endl; }
 };
 
 struct B: public Base {
-	void print() { std::cout << "struct B" << std::endl; }
+	B(double b): name("struct B"), b(b) {}
+
+	std::string name;
+	double b;
+
+	void print() const { std::cout << this << name << b << std::endl; }
 };
 
-template <typename T = void, typename... Ts>
+template <template <typename T,typename> class Container = std::vector, typename T = void, typename... Ts>
 struct CompositeVector {
-	CompositeVector(std::vector<T> t, std::vector<Ts>... ts): vec(t), /*it(vec.end()),*/ composite(ts...) {}
-	std::vector<T> vec;
+	CompositeVector(Container<T, std::allocator<T>>& t, Container<Ts, std::allocator<Ts>>&... ts): container(t), composite(ts...) {}
 
-	CompositeVector<Ts...> composite;
+	Container<T, std::allocator<T>> container;
+	CompositeVector<Container, Ts...> composite;
 
-	template <typename CommonBase>
-	void visit(std::function<void(CommonBase&)> f) {
-		{
-			for (auto it = vec.begin(); it != vec.end(); ++it) {
-				f(*it);
-			}
-		}
-
-		composite.visit(f);
+	void visit(const std::function<void(const T&)>& f, const std::function<void(const Ts&)>... fs) const {
+		std::for_each(container.cbegin(), container.cend(), f);
+		composite.visit(fs...);
 	}
 };
 
-template <>
-struct CompositeVector<void> {
-	template <typename T>
-	void visit(std::function<void(T&)>) {}
+template <template <typename,typename> class Container>
+struct CompositeVector<Container, void> {
+	void visit() const {}
 };
 
-int main() {
-	std::vector<A> vecA({A()});
-	std::vector<B> vecB({B()});
-	auto vec = CompositeVector<A, B>(vecA, vecB);
+#include <vector>
+#include <list>
+#include <deque>
 
-	vec.visit<Base>([](Base& base){ base.print(); });
+template <typename T, typename A = std::allocator<T>>
+using container = std::list<T, A>;
+
+int main() {
+
+	container<A> vecA;
+	//vecA.reserve(20);
+
+	container<B> vecB;
+	//vecB.reserve(20);
+
+	std::vector<A*> vecA2;
+	std::vector<B*> vecB2;
+
+	for (int i = 0; i < 20; ++i) {
+		for (int j = 32001; j > 0; --j) vecA2.push_back(new A(j));
+		vecA.push_back(*vecA2[32001*i + i]);
+	}
+
+	for (int i = 0; i < 20; ++i) {
+		for (int j = 32001; j > 0; --j) vecB2.push_back(new B(0.33*j));
+		vecB.push_back(*vecB2[32001*i + i]);
+	}
+
+	const auto vec = CompositeVector<container, A, B>(vecA, vecB);
+
+	const auto al = [](const A& a)->void{ a.print(); };
+	const auto bl = [](const B& b)->void{ b.print(); };
+
+	for (auto i = 50000; i > 0; --i)
+		vec.visit(al, bl);
 
 	return 0;
 }
