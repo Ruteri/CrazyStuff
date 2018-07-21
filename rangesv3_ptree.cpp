b3055834e68ca223b32b4188063e5b6b22e87b12
Mateusz Morusiewicz <mmorusiewicz@newterm.pl>
2018-07-21 15:56:06 +0200

    Ptree: added reference based ptree

--- rangesv3_ptree.cpp
+++ rangesv3_ptree.cpp
@@ -1,6 +1,7 @@
 #include <sstream>
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
+#include <boost/property_tree/json_parser/detail/write.hpp>
 
 #include <utility>
 #include <vector>
@@ -131,6 +132,124 @@ using ptree_iterator_based = basic_ptree_iterator_based<std::string>;
 
 #include <iostream>
 
+struct Alarm {
+	using key_type = std::string;
+	size_t id;
+	size_t raise_time;
+	std::string msg;
+};
+
+struct AlarmSource {
+	std::string name;
+	std::deque<std::pair<std::string, Alarm>> alarms;
+};
+
+struct basic_alarm_ptree { // TODO: make container type a template
+	using key_type = std::string;
+	using data_type = Alarm;
+
+	using container_type = typename std::deque<std::pair<key_type, Alarm>>;
+	using const_iterator = typename container_type::const_iterator;
+
+	boost::optional<AlarmSource&> alarm_source;
+	container_type empty_container;
+
+	const_iterator begin() const {
+		if (alarm_source)
+			return (*alarm_source).alarms.begin();
+		return  empty_container.begin();
+	}
+
+	const_iterator end() const {
+		if (alarm_source)
+			return (*alarm_source).alarms.end();
+		return  empty_container.end();
+	}
+	
+	basic_alarm_ptree() = default;
+	basic_alarm_ptree(AlarmSource& as): alarm_source(as) {}
+
+	/* serialization (access) */
+	template <typename Str>
+	Str get_value() const { return ""; }
+
+	bool empty() const { return size() == 0; }
+
+	size_t count(const key_type& key) const {
+		return key.empty()? size() : 0;
+	}
+
+	size_t size() const { return alarm_source? (*alarm_source).alarms.size() : 0; }
+};
+
+struct basic_alarm_ptree_holder { // TODO: make container type a template
+	using key_type = std::string;
+	using data_type = basic_alarm_ptree;
+
+	using container_type = typename std::deque<std::pair<key_type, data_type>>;
+	using const_iterator = typename container_type::const_iterator;
+
+	container_type alarm_sources;
+
+	const_iterator begin() const { return alarm_sources.begin(); }
+	const_iterator end() const { return alarm_sources.end(); }
+	
+	key_type m_data;
+	basic_alarm_ptree_holder(const key_type& data = key_type()): m_data(data) {}
+
+	/* serialization (access) */
+	template <typename Str>
+	Str get_value() const { return m_data; }
+
+	bool empty() const { return alarm_sources.empty(); }
+
+	size_t count(const key_type& key) const {
+		return key.empty()? size() : 0;
+	}
+
+	size_t size() const { return alarm_sources.size(); }
+
+	// TODO: add modifiers
+};
+
+namespace boost { namespace property_tree { namespace json_parser {
+    template <>
+    void write_json_helper(std::basic_ostream<typename Alarm::key_type::value_type> &stream, 
+                           const Alarm &pt,
+                           int indent, bool pretty)
+    {
+		using Ch = char;
+		using Str = std::string;
+
+		stream << Ch('{');
+
+		if (pretty) stream << Ch('\n') << Str(4 * (indent + 1), Ch(' '));
+		stream << Ch('"') << Str("id") << Ch('"') << Ch(':');
+		if (pretty) stream << Ch(' ');
+		stream << pt.id << Ch(',');
+
+		if (pretty) stream << Ch('\n') << Str(4 * (indent + 1), Ch(' '));
+		stream << Ch('"') << Str("raise_time") << Ch('"') << Ch(':');
+		if (pretty) stream << Ch(' ');
+		stream << pt.raise_time << Ch(',');
+
+		if (pretty) stream << Ch('\n') << Str(4 * (indent + 1), Ch(' '));
+		stream << Ch('"') << Str("msg") << Ch('"') << Ch(':');
+		if (pretty) stream << Ch(' ');
+		stream << Str(pt.msg);
+
+		if (pretty) stream << Ch('\n');
+
+		if (pretty) stream << Str(4 * indent, Ch(' '));
+		stream << Ch('}');
+    }
+
+    template <>
+    bool verify_json(const Alarm &, int) { return true; }
+
+}}} // ns boost::property_tree::json_parser
+
+
 
 int main() {
 
@@ -158,6 +277,22 @@ int main() {
 		pt::write_json (buf, pt, true);
 		std::cout << buf.str();
 	}
+
+	{
+		AlarmSource as1;
+		as1.alarms.emplace_back("", Alarm{1, 100, "alarm1"});
+		as1.alarms.emplace_back("", Alarm{2, 113, "alarm2"});
+
+		AlarmSource as2;
+		as2.alarms.emplace_back("", Alarm{1, 103, "alarm1"});
+		as2.alarms.emplace_back("", Alarm{2, 121, "alarm2"});
+
+		basic_alarm_ptree_holder holder;
+		holder.alarm_sources.push_back({"alarm source 1", basic_alarm_ptree(as1)});
+		holder.alarm_sources.push_back({"alarm source 2", basic_alarm_ptree(as2)});
+		pt::write_json(std::cout, holder, true);
+	}
+	
 
 	return 0;
 }
