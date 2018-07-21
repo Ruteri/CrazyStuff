558fa2fe5a98cd9fe409ecff18f14414eef5d7e1
Mateusz Morusiewicz <mmorusiewicz@newterm.pl>
2018-07-21 19:12:08 +0200

    Ptree: reference based approach with type erased multitype holder

--- rangesv3_ptree.cpp
+++ rangesv3_ptree.cpp
@@ -144,6 +144,34 @@ struct AlarmSource {
 	std::deque<std::pair<std::string, Alarm>> alarms;
 };
 
+struct te_variant_ptree_holder {
+	using key_type = std::string;
+    virtual void write_json_helper(std::basic_ostream<key_type::value_type> &stream, int indent, bool pretty) const = 0;
+
+    virtual bool verify_json(int) const = 0;
+};
+
+template <typename T>
+struct variant_ptree_holder: public te_variant_ptree_holder {
+	using te_variant_ptree_holder::key_type;
+    void write_json_helper(std::basic_ostream<key_type::value_type> &stream, int indent, bool pretty) const override {
+		boost::property_tree::json_parser::write_json_helper(stream, obj, indent, pretty);
+	}
+
+    bool verify_json(int indent) const override {
+		return boost::property_tree::json_parser::verify_json(obj, indent);
+	}
+
+	T& obj;
+
+	/* template <typename... Args> // perfect forwarding maybe?
+	variant_ptree_holder(Args... args): obj(T(args...)) {} // use by-value (T obj) */
+
+	variant_ptree_holder(T& _obj): obj(_obj) {}
+
+};
+
+// adapter class
 struct basic_alarm_ptree { // TODO: make container type a template
 	using key_type = std::string;
 	using data_type = Alarm;
@@ -151,23 +179,23 @@ struct basic_alarm_ptree { // TODO: make container type a template
 	using container_type = typename std::deque<std::pair<key_type, Alarm>>;
 	using const_iterator = typename container_type::const_iterator;
 
-	boost::optional<AlarmSource&> alarm_source;
+	boost::optional<std::reference_wrapper<AlarmSource>> alarm_source;
 	container_type empty_container;
 
 	const_iterator begin() const {
 		if (alarm_source)
-			return (*alarm_source).alarms.begin();
+			return (*alarm_source).get().alarms.begin();
 		return  empty_container.begin();
 	}
 
 	const_iterator end() const {
 		if (alarm_source)
-			return (*alarm_source).alarms.end();
+			return (*alarm_source).get().alarms.end();
 		return  empty_container.end();
 	}
 	
 	basic_alarm_ptree() = default;
-	basic_alarm_ptree(AlarmSource& as): alarm_source(as) {}
+	basic_alarm_ptree(AlarmSource& as): alarm_source(std::ref(as)) {}
 
 	/* serialization (access) */
 	template <typename Str>
@@ -179,42 +207,79 @@ struct basic_alarm_ptree { // TODO: make container type a template
 		return key.empty()? size() : 0;
 	}
 
-	size_t size() const { return alarm_source? (*alarm_source).alarms.size() : 0; }
+	size_t size() const { return alarm_source? (*alarm_source).get().alarms.size() : 0; }
 };
 
-struct basic_alarm_ptree_holder { // TODO: make container type a template
-	using key_type = std::string;
-	using data_type = basic_alarm_ptree;
+#include <functional>
 
-	using container_type = typename std::deque<std::pair<key_type, data_type>>;
+// ignore this, workaround for key_type in boost's function specialization
+namespace std {
+
+template <>
+struct _Reference_wrapper_base<te_variant_ptree_holder>
+    : _Reference_wrapper_base_impl<
+      __has_argument_type<te_variant_ptree_holder>::value,
+      __has_first_argument_type<te_variant_ptree_holder>::value
+      && __has_second_argument_type<te_variant_ptree_holder>::value,
+      te_variant_ptree_holder>
+{
+	using key_type = std::string;
+};
+
+} // ns std
+
+struct basic_ptree_holder { // TODO: make container type a template
+	using key_type = std::string;
+	using path_type = std::string;
+	using data_type = std::string;
+
+	using held_type = std::reference_wrapper<te_variant_ptree_holder>;
+	using container_type = typename std::deque<std::pair<key_type, held_type>>;
 	using const_iterator = typename container_type::const_iterator;
 
-	container_type alarm_sources;
+	container_type m_children;
 
-	const_iterator begin() const { return alarm_sources.begin(); }
-	const_iterator end() const { return alarm_sources.end(); }
+	const_iterator begin() const { return m_children.begin(); }
+	const_iterator end() const { return m_children.end(); }
 	
-	key_type m_data;
-	basic_alarm_ptree_holder(const key_type& data = key_type()): m_data(data) {}
+	data_type m_data;
+	basic_ptree_holder(const data_type& data = data_type()): m_data(data) {}
 
 	/* serialization (access) */
 	template <typename Str>
 	Str get_value() const { return m_data; }
 
-	bool empty() const { return alarm_sources.empty(); }
+	bool empty() const { return m_children.empty(); }
 
 	size_t count(const key_type& key) const {
 		return key.empty()? size() : 0;
 	}
 
-	size_t size() const { return alarm_sources.size(); }
+	size_t size() const { return m_children.size(); }
 
-	// TODO: add modifiers
+	/* modifiers */
+	void put_value(const data_type &value) {
+		m_data = value;
+	}
+
+	template <typename T>
+	void put_child(const path_type &path, T& value) {
+		put_child(path, held_type { std::ref(value) });
+	}
+
+	void put_child(const path_type &path, const held_type &value) {
+		m_children.push_back({path, value});
+	}
+
+	void put(const path_type &path, const held_type &value) {
+		held_type new_node(value);
+		put_child(path, new_node);
+	}
 };
 
 namespace boost { namespace property_tree { namespace json_parser {
     template <>
-    void write_json_helper(std::basic_ostream<typename Alarm::key_type::value_type> &stream, 
+    void write_json_helper(std::basic_ostream<std::string::value_type> &stream, 
                            const Alarm &pt,
                            int indent, bool pretty)
     {
@@ -247,13 +312,26 @@ namespace boost { namespace property_tree { namespace json_parser {
     template <>
     bool verify_json(const Alarm &, int) { return true; }
 
-}}} // ns boost::property_tree::json_parser
+    template <>
+    void write_json_helper(std::basic_ostream<std::string::value_type> &stream, 
+                           const std::reference_wrapper<te_variant_ptree_holder> &pt,
+                           int indent, bool pretty)
+    {
+		pt.get().write_json_helper(stream, indent, pretty);
+	}
 
+    template <>
+    bool verify_json(const std::reference_wrapper<te_variant_ptree_holder> &pt, int indent) {
+		return pt.get().verify_json(indent);
+	}
+
+
+}}} // ns boost::property_tree::json_parser
 
 
 int main() {
 
-	{
+	{ /*
 		ptree_copybased pt2;
 		ptree_copybased pt3;
 		pt3.put_value("bar");
@@ -264,10 +342,10 @@ int main() {
 		std::ostringstream buf; 
 		pt::write_json (buf, pt2, true);
 		std::cout << buf.str();
-	}
+	*/ }
 
 
-	{
+	{ /*
 		ptree_iterator_based pt;
 		std::deque<std::pair<std::string, ptree_iterator_based>> v1{{"foo", ptree_iterator_based("bar")}};
 		std::deque<std::pair<std::string, ptree_iterator_based>> v2{{"foo", ptree_iterator_based("baz")}};
@@ -276,23 +354,44 @@ int main() {
 		std::ostringstream buf; 
 		pt::write_json (buf, pt, true);
 		std::cout << buf.str();
-	}
+	*/ }
+
 
 	{
+		basic_ptree_holder holder;
 		AlarmSource as1;
-		as1.alarms.emplace_back("", Alarm{1, 100, "alarm1"});
-		as1.alarms.emplace_back("", Alarm{2, 113, "alarm2"});
+		as1.alarms.emplace_back("a", Alarm{1, 100, "alarm1"});
+		as1.alarms.emplace_back("b", Alarm{2, 113, "alarm2"});
 
 		AlarmSource as2;
-		as2.alarms.emplace_back("", Alarm{1, 103, "alarm1"});
-		as2.alarms.emplace_back("", Alarm{2, 121, "alarm2"});
+		as2.alarms.emplace_back("c", Alarm{1, 103, "alarm1"});
+		as2.alarms.emplace_back("d", Alarm{2, 121, "alarm2"});
 
-		basic_alarm_ptree_holder holder;
-		holder.alarm_sources.push_back({"alarm source 1", basic_alarm_ptree(as1)});
-		holder.alarm_sources.push_back({"alarm source 2", basic_alarm_ptree(as2)});
+		AlarmSource as3;
+
+		basic_alarm_ptree ap1(as1);
+		variant_ptree_holder<basic_alarm_ptree> vp1(ap1);
+		holder.put_child("alarm source 1", vp1);
+
+		basic_alarm_ptree ap2(as2);
+		variant_ptree_holder<basic_alarm_ptree> vp2(ap2);
+		holder.put_child("alarm source 1", vp2);
+
+		basic_alarm_ptree ap3(as3);
+		variant_ptree_holder<basic_alarm_ptree> vp3(ap3);
+		holder.put_child("alarm source 1", vp3);
+
+		pt::ptree pt4;
+		variant_ptree_holder<pt::ptree> vp4(pt4);
+		holder.put_child("some other json stuff", vp4);
+		pt4.put_value("some other value");
+
 		pt::write_json(std::cout, holder, true);
 	}
 	
+	{
+
+	}
 
 	return 0;
 }
